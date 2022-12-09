df6315323783e5c86473bc6c48eeb9697bd4bfb1
gassysoil <zhangyanjohn@gmail.com>
2022-12-08 22:46:10 -0800

    after chapter 271

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { Route, Routes } from "react-router-dom";
+import Welcome from "./pages/Welcome";
+import Products from "./pages/Products";
+import ProductDetail from "./pages/ProductDetail";
+import MainHeader from "./components/MainHeader";
+
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <MainHeader></MainHeader>
+      <main>
+        <Routes>
+          <Route path="/welcome" element={<Welcome></Welcome>}></Route>
+          <Route path="/products" element={<Products></Products>}></Route>
+          <Route
+            path="/product_detail/:productID"
+            element={<ProductDetail></ProductDetail>}
+          ></Route>
+        </Routes>
+      </main>
     </div>
   );
 }
